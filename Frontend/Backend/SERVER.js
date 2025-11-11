50ed3f4274b3c0c737f7903621f31722157dff23
niatapppurpose-APPs <niatapppurpose@gmail.com>
2025-11-11 08:45:34 +0000

    Resolved

---
+++ Frontend/Backend/SERVER.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
+
+dotenv.config();  // Load the environment variables
+
+MongoClient.connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('DB Connected Successfully');
+  })
+  .catch((error) => {
+    console.log('Error', error);
+  });
+
+const app = express();
+app.listen(3000, () => {
+  console.log('Server started');
+});
---
+++ js
@@ -0,0 +1,6 @@
+[33m[nodemon] 3.1.10[39m
+[33m[nodemon] to restart at any time, enter `rs`[39m
+[33m[nodemon] watching path(s): *.*[39m
+[33m[nodemon] watching extensions: js,mjs,cjs,json[39m
+[32m[nodemon] starting `node server.js SERVER`[39m
+[31m[nodemon] app crashed - waiting for file changes before starting...[39m
